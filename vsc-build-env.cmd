cf536047a2384826b7671f6b3c156e714a14ba91
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-08-17 19:15:50 -0700

    Make the build environment launch script a bit more robust

--- vsc-build-env.cmd
+++ vsc-build-env.cmd
@@ -1,10 +1,13 @@
 @echo off
+setlocal EnableExtensions
 
 :: Assume build environment is already setup if msbuild can be located
 where msbuild >nul 2>nul && exit /b 0
 
 :: Allow the path to vsdevcmd to be provided by our caller
-if exist "%vsdevcmd%" "%vsdevcmd%"
+if exist "%vsdevcmd%" (
+    goto:$LaunchDevCmd
+)
 :: If we're still running, must be no vsdevcmd
 
 if "%ProgramFiles(x86)%"=="" set ProgramFiles(x86)=%ProgramFiles%
@@ -15,11 +18,28 @@ if not exist %vswhere% (
 )
 
 :: This should work for Visual Studio
-for /f "usebackq delims=" %%i in (`%vswhere% -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -find *\Tools\vsdevcmd.bat`) do "%%i"
+for /f "usebackq delims=" %%i in (`%vswhere% -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -find *\Tools\vsdevcmd.bat`) do (
+    set "vsdevcmd=%%i"
+    goto:$LaunchDevCmd
+)
+
 :: This should work with Visual Studio Build Tools
-for /f "usebackq delims=" %%i in (`%vswhere% -latest -products * -requires Microsoft.VisualStudio.Workload.VCTools -find *\Tools\vsdevcmd.bat`) do "%%i"
+for /f "usebackq delims=" %%i in (`%vswhere% -latest -products * -requires Microsoft.VisualStudio.Workload.VCTools -find *\Tools\vsdevcmd.bat`) do (
+    set "vsdevcmd=%%i"
+    goto:$LaunchDevCmd
+)
+
 :: As a last resort, try without specifying the required workload
-for /f "usebackq delims=" %%i in (`%vswhere% -latest -products * -find *\Tools\vsdevcmd.bat`) do "%%i"
+for /f "usebackq delims=" %%i in (`%vswhere% -latest -products * -find *\Tools\vsdevcmd.bat`) do (
+    set "vsdevcmd=%%i"
+    goto:$LaunchDevCmd
+)
+
 :: If we're still running, vsdevcmd wasn't executed
-echo Unable to locate build tools.
+echo [ERROR] Unable to locate build tools.
 exit 1
+
+:$LaunchDevCmd
+echo Found Visual Studio build tools: "%vsdevcmd%"
+echo cmd /d /k "%vsdevcmd%"
+cmd /d /k "%vsdevcmd%"
