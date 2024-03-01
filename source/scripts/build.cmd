8b7aed0810bdbdfd2645c9c824b938fcac27377a
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-01 12:04:49 -0800

    Build script improvements

--- source/scripts/build.cmd
+++ source/scripts/build.cmd
@@ -3,6 +3,8 @@ goto:$Main
 
 :$Main
 setlocal
-    devenv "%~dp0..\..\AutoHotkeyx.sln" /Build >"%~dp0..\..\.build\devenv_log.txt" 2>&1
-    msbuild /m /t:Rebuild /p:Configuration="Release" /p:Platform="x64" "%~dp0..\..\AutoHotkeyx.sln" >"%~dp0..\..\.build\msbuild_log.txt" 2>&1
+    set "root=%~dp0..\.."
+    call "%root%\vsc-build-env.cmd"
+    devenv "%root%\AutoHotkeyx.sln" /Build >"%root%\.build\devenv_log.txt" 2>&1
+    msbuild /m /t:Rebuild /p:Configuration="Release" /p:Platform="x64" "%root%\AutoHotkeyx.sln" >"%root%\.build\msbuild_log.txt" 2>&1
 endlocal & exit /b %ERRORLEVEL%
